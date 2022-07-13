46ac8feacdee18b6157461d0c523190d459a061d
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-07-13 16:01:36 +0000

    Deploying to gh-pages from @ shuriuss/iceCream@ff7ae9a4029342f3ff48cdeda48a08363139ad65 🚀

---
+++ index.4a66cd38.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[open-btn]"),closeModalBtn:document.querySelector("[close-btn]"),modal:document.querySelector("[popup]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.4a66cd38.js.map
---
+++ index.db6c8315.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelectorAll("[data-modal-opens]"),closeModalBtn:document.querySelector("[data-modal-closes]"),modal:document.querySelector("[data-modals]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.forEach((function(e){e.addEventListener("click",o)})),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.db6c8315.js.map
